554c2974341beb4b869b6a44a5217ab3db8866e6
PARDEEP KAUR <pkaur84@aol.com>
2020-06-12 18:37:11 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
-class Artists < ActiveRecord::Base
+class Artist < ActiveRecord::Base
   
+  has_many :songs
+  has_many :genres, through: :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
-class Songs < ActiveRecord::Base
+class Song < ActiveRecord::Base
   
+  belongs_to :artist
+  belongs_to :genre
 end
